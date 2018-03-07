6853c3518c21e7a362e3ea91f823dc8460bf4d08
stewartpressney <stewartpressney@gmail.com>
2018-03-06 18:04:36 -0800

    sql migration with knex

---
+++ knexfile.js
@@ -0,0 +1,9 @@
+const settings = require("./settings");
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: settings
+  }
+
+};
---
+++ migrations/20180307015600_create_milestones.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('milestones', function(table){
+      table.increments('id').primary();
+      table.string('description');
+      table.string('date_achieved');
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('milestones')
+  ])
+};
